91e5c71cd5cf4666faabf453b998e2bea8ea1bb1
Anirudh Bhat <anirudh.b.123@gmail.com>
2018-12-06 21:09:30 +0530

    added number buttons and input is working

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,23 +20,57 @@ type Props = {};
 export default class App extends Component<Props> {
 
   state = {
-    textValue: 'default'
+    input: ''
   }
 
-  handleInputChange = (newText) => {
+  onNumberClick = (recievedNumber) => {
+    if(recievedNumber == '='){
+      return this.calculateResult();
+    }
     this.setState({
-      textValue: newText
+      input: this.state.input+recievedNumber
     })
   }
 
   render() {
+
+    let numRows = [];
+    let nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['.', 0, '=']]
+    for(let i=0; i<4; i++){
+      let row = [];
+      for(let j=0; j<3; j++){
+        row.push(
+          <TouchableOpacity style={styles.btn} onPress={() => this.onNumberClick(nums[i][j])}>
+            <Text style={styles.btnText}>{nums[i][j]}</Text>
+          </TouchableOpacity>
+        );
+      }
+      numRows.push(<View style={styles.row}>{row}</View>)
+    }
+
+    let operatorCols = [];
+    let operators = ['+', '-', '*', '/'];
+    for(let i=0; i<4; i++){
+      operatorCols.push(
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>{operators[i]}</Text>
+        </TouchableOpacity>
+      )
+    }
+
     return (
       <View style={styles.container}>
           <View style={styles.result}></View>
-          <View style={styles.inputs}></View>
+          <View style={styles.inputs}>
+            <Text>{this.state.input}</Text>
+          </View>
           <View style={styles.buttons}>
-            <View style={styles.numbers}></View>
-            <View style={styles.operators}></View>
+            <View style={styles.numbers}>
+              {numRows}
+            </View>
+            <View style={styles.operators}>
+              {operatorCols}
+            </View>
           </View>
       </View>
     );
@@ -51,6 +85,15 @@ const styles = StyleSheet.create({
     flex: 2,
     backgroundColor: '#FFFFFF'
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  column: {
+    flex: 1
+  },
   inputs: {
     flex: 1,
     backgroundColor: '#CCCCCC'
@@ -58,6 +101,15 @@ const styles = StyleSheet.create({
   buttons: {
     flex: 7,
     flexDirection: 'row'
+  },
+  btn: {
+    flex: 1,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  btnText: {
+    fontSize: 25
   },
   numbers: {
     flex: 3,
